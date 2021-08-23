df83f5519010029254912df77656f42bc5e1d508
rami-elementor <ramiy@elementor.com>
2021-08-23 11:53:17 +0300

    Error fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -35,7 +35,7 @@ module.exports = {
     ['link', { rel: 'apple-touch-icon', href: '/assets/img/logo.svg' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'msapplication-TileImage', content: '/assets/img/logo.svg' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
