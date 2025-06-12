7a67778228d51f35a4ef71a16f54a14d8aa7528f
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2025-06-12 05:49:39 -0700

    Hooks: Mask Shapes (#314)

--- src/.vuepress/sidebars/hooks.js
+++ src/.vuepress/sidebars/hooks.js
@@ -15,6 +15,7 @@ module.exports = [
       'php',
       'placeholder-image',
       'shapes',
+      'masks',
       'frontend-content',
       'render-frontend-elements',
       'render-widget-content',
